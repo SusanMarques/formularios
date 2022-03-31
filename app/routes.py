f61eb990f4d263bb85159b61af51c5b108156596
SusanMarques <susankarin.info@gmail.com>
2022-03-31 20:15:51 -0300

    primeiro commit

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Susan'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/contato')
+def contato():
+    user = {'username': 'Susan'}
+    return render_template('contatos.html', title='contato', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',title='sign in',form=form)
